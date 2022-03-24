e6c32c0304d531085c9c5b8c54facad52afa58fe
imaneEmi <imane.elaimani@gmail.com>
2022-03-24 14:45:04 +0100

    add: custom request

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const app = express();
 const SERVER_PORT = 3000
 
 
-const GET_UNIVERSITIES_URL = 'http://universities.hipolabs.com/search?country=morocco';
+const GET_UNIVERSITIES_URL = 'http://universities.hipolabs.com/search?country=';
 
 app.set("view engine", "ejs");
 
 app.listen(SERVER_PORT, () => {
-    app.get('/universities', async (req ,res) => { 
+    app.get('/universities/:country', async(req, res) => {
         try {
-            const { data } =  await axios.get(GET_UNIVERSITIES_URL);
+            const { data } = await axios.get(GET_UNIVERSITIES_URL + req.params.country);
             res.render("index", {
-              universities: data,
-              });
-          } catch(ex) {
+                universities: data,
+            });
+        } catch (ex) {
             res.status(500).send(ex.data);
-          }
-          
-      });
-      
+        }
+
+    });
+
 });
