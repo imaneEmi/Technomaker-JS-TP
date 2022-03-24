7e393f8720d027d1d393e5d21b080e05d817dd94
user <user@user.com>
2022-03-24 11:51:54 +0100

    ejs commit

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ const END_POINT = 'http://universities.hipolabs.com/search?country=morocco';
 
 var app = express();
 app.use(cors( { origin: '*'}));
+app.set("view engine", "ejs");
 app.listen(3000, () => {
     app.get('/', async (req, res) => { 
         try {
             const { data } =  await axios.get(END_POINT);
-            res.status(200).send(data);
+            res.render("index", {
+              universities: data,
+              });
           } catch(ex) {
             res.status(500).send(ex.data);
           }
+          
       });
- console.log("Server running on port 3000");
+      
 });
